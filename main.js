39c9833c064d7814082c39292d46ef2ca482c401
Jack Mathias <73205188+HumanoidsYoutube@users.noreply.github.com>
2020-12-24 11:36:26 -0600

    Updates: Basic Outline

---
+++ main.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("nav").style.width = "20%";
+  document.getElementById("full").style.marginLeft = "20%";
+}
+
+function closeNav() {
+  document.getElementById("nav").style.width = "0";
+  document.getElementById("full").style.marginLeft= "0";
+}
